f69d2add9d1faa5a96d34694f992fdd7ebcebaa6
DhruviViraniMDTech <dhruviviranimdtech@gmail.com>
2025-07-30 13:26:01 +0530

    update based url

--- src/redux/Api.ts
+++ src/redux/Api.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.29.84:5000/api/', // Updated baseURL
+  baseURL: 'https://ar-plant-api.onrender.com/api/',
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-export default api
+export default api;
